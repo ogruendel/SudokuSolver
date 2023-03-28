7918e448e270df891eea6b92507e0751a063bbfb
Oliver <oliver.gruendel2004@gmail.com>
2023-03-28 16:26:56 +0200

    check legal value in boxes, refactor

--- backtracking/backtracking.py
+++ backtracking/backtracking.py
@@ -1,49 +1,69 @@
 def fix_positions(board):
     fixed_positions = []
-    for row_pos, row in enumerate(board):
-        for col_pos, num in enumerate(row):
+    for pos_y, row in enumerate(board):
+        for pos_x, num in enumerate(row):
             fixed_pos = []
             if num != 0:
-                fixed_pos.append(row_pos)
-                fixed_pos.append(col_pos)
+                fixed_pos.append(pos_x)
+                fixed_pos.append(pos_y)
                 fixed_positions.append(fixed_pos)
     return fixed_positions
 
 
-def solve(board, fixed):
-    for row_pos, row in enumerate(board):
-        for col_pos, num in enumerate(row):
-            if [row_pos, col_pos] not in fixed:
-                for i in range(1, 10):
-                    # TODO
-                    return
+def check_legal(board, pos_x, pos_y, val):
+    check_legal_in_row(board, pos_y, val)
+    check_legal_in_col(board, pos_x, val)
+    check_legal_in_box(board, pos_x, pos_y, val)
 
 
-def check_legal(board, row, col):
-    check_legal_in_row(board, row, col)
-    check_legal_in_col(board, row, col)
-    check_legal_in_box(board, row, col)
-
-
-def check_legal_in_row(board, pos_row, pos_col):
-    to_check = board[pos_row][pos_col]
-    if to_check == 0 or board[pos_row].count(to_check) != 1:
+def check_legal_in_row(board, pos_y, val):
+    if board[pos_y].count(val) >= 1:
         return False
     else:
         return True
 
 
-def check_legal_in_col(board, pos_row, pos_col):
-    to_check = board[pos_row][pos_col]
+def check_legal_in_col(board, pos_x, val):
     col = []
-    for i in range(0, 9):
-        col.append(board[i][pos_col])
-    if to_check == 0 or col.count(to_check) != 1:
+    for i in range(9):
+        col.append(board[i][pos_x])
+    if col.count(val) >= 1:
         return False
     else:
         return True
 
 
-def check_legal_in_box(board, pos_row, pos_col):
-    box = []
+def check_legal_in_box(board, pos_x, pos_y, val):
     # TODO
+    box = []
+    if pos_y / 3 < 1:
+        y = 0
+    elif pos_y / 3 < 2:
+        y = 3
+    else:
+        y = 6
+
+    if pos_x / 3 < 1:
+        x = 0
+    elif pos_x / 3 < 2:
+        x = 3
+    else:
+        x = 6
+
+    for i in range(3):
+        for j in range(3):
+            box.append(board[i + y][j + x])
+    if val in box:
+        return False
+    else:
+        return True
+
+
+def solve(board, fixed):
+    # TODO
+    for pos_x in range(9):
+        for pos_y in range(9):
+            if [pos_x, pos_y] not in fixed:
+                for num in range(1, 10):
+                    if check_legal(board, pos_x, pos_y, num):
+                        board[pos_x][pos_y] = num
--- backtracking/test_backtracking.py
+++ backtracking/test_backtracking.py
@@ -1,5 +1,5 @@
 import unittest
-import backtracking
+import backtracking as b
 
 
 class TestBacktracking(unittest.TestCase):
@@ -16,14 +16,14 @@ class TestBacktracking(unittest.TestCase):
             [5, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
 
-        r = backtracking.check_legal_in_row(board_1, 0, 0)
-        self.assertEqual(r, True)
-        r = backtracking.check_legal_in_row(board_1, 1, 5)
-        self.assertEqual(r, True)
-        r = backtracking.check_legal_in_row(board_1, 2, 1)
+        r = b.check_legal_in_row(board_1, 0, 1)
         self.assertEqual(r, False)
-        r = backtracking.check_legal_in_row(board_1, 6, 6)
+        r = b.check_legal_in_row(board_1, 1, 4)
         self.assertEqual(r, False)
+        r = b.check_legal_in_row(board_1, 2, 4)
+        self.assertEqual(r, True)
+        r = b.check_legal_in_row(board_1, 6, 9)
+        self.assertEqual(r, True)
 
     def test_check_legal_in_col(self):
         board_1 = [
@@ -37,11 +37,34 @@ class TestBacktracking(unittest.TestCase):
             [6, 0, 4, 5, 5, 9, 0, 6, 7],
             [7, 4, 5, 8, 8, 0, 0, 0, 0],
         ]
-        r = backtracking.check_legal_in_col(board_1, 0, 0)
+
+        r = b.check_legal_in_col(board_1, 0, 1)
         self.assertEqual(r, False)
-        r = backtracking.check_legal_in_col(board_1, 4, 8)
+        r = b.check_legal_in_col(board_1, 4, 1)
         self.assertEqual(r, True)
-        r = backtracking.check_legal_in_col(board_1, 1, 5)
+        r = b.check_legal_in_col(board_1, 1, 5)
         self.assertEqual(r, True)
-        r = backtracking.check_legal_in_col(board_1, 2, 5)
+        r = b.check_legal_in_col(board_1, 2, 4)
+        self.assertEqual(r, False)
+
+    def test_check_legal_in_box(self):
+        board_1 = [
+            [0, 0, 7, 0, 0, 0, 4, 0, 0],
+            [0, 0, 0, 3, 7, 0, 9, 0, 8],
+            [0, 9, 0, 0, 0, 5, 7, 6, 0],
+            [0, 0, 1, 8, 0, 0, 5, 0, 0],
+            [2, 7, 5, 6, 4, 0, 8, 0, 0],
+            [9, 4, 8, 5, 3, 0, 6, 7, 2],
+            [0, 0, 3, 0, 9, 0, 0, 0, 0],
+            [0, 5, 4, 2, 0, 0, 0, 9, 6],
+            [1, 0, 9, 7, 0, 3, 0, 0, 0]
+        ]
+
+        r = b.check_legal_in_box(board_1, 2, 1, 4)
+        self.assertEqual(r, True)
+        r = b.check_legal_in_box(board_1, 8, 4, 1)
+        self.assertEqual(r, True)
+        r = b.check_legal_in_box(board_1, 3, 3, 6)
+        self.assertEqual(r, False)
+        r = b.check_legal_in_box(board_1, 8, 0, 6)
         self.assertEqual(r, False)

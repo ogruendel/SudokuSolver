cefc8bf77582fa893f0520ccae577850edb71fbd
Oliver <oliver.gruendel2004@gmail.com>
2023-03-28 19:55:56 +0200

    Backtracking implemented

--- backtracking/backtracking.py
+++ backtracking/backtracking.py
@@ -1,23 +1,28 @@
-def fix_positions(board):
-    fixed_positions = []
-    for pos_y, row in enumerate(board):
-        for pos_x, num in enumerate(row):
-            fixed_pos = []
+def print_board(board):
+    for i, row in enumerate(board):
+        x = ""
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j, num in enumerate(row):
             if num != 0:
-                fixed_pos.append(pos_x)
-                fixed_pos.append(pos_y)
-                fixed_positions.append(fixed_pos)
-    return fixed_positions
+                x += str(num) + " "
+            else:
+                x += "  "
+            if (j + 1) % 3 == 0 and j != 8:
+                x += "| "
+        print(x)
+    print('\n')
 
 
 def check_legal(board, pos_x, pos_y, val):
-    check_legal_in_row(board, pos_y, val)
-    check_legal_in_col(board, pos_x, val)
-    check_legal_in_box(board, pos_x, pos_y, val)
+    return check_legal_in_row(board, pos_y, val) and check_legal_in_col(board, pos_x, val) and check_legal_in_box(board,
+                                                                                                                  pos_x,
+                                                                                                                  pos_y,
+                                                                                                                  val)
 
 
 def check_legal_in_row(board, pos_y, val):
-    if board[pos_y].count(val) >= 1:
+    if board[pos_y].count(val) == 1:
         return False
     else:
         return True
@@ -27,14 +32,13 @@ def check_legal_in_col(board, pos_x, val):
     col = []
     for i in range(9):
         col.append(board[i][pos_x])
-    if col.count(val) >= 1:
+    if col.count(val) == 1:
         return False
     else:
         return True
 
 
 def check_legal_in_box(board, pos_x, pos_y, val):
-    # TODO
     box = []
     if pos_y / 3 < 1:
         y = 0
@@ -59,11 +63,24 @@ def check_legal_in_box(board, pos_x, pos_y, val):
         return True
 
 
-def solve(board, fixed):
-    # TODO
-    for pos_x in range(9):
-        for pos_y in range(9):
-            if [pos_x, pos_y] not in fixed:
+def is_solved(board):
+    for row in board:
+        for i in range(1, 10):
+            if row.count(i) != 1 or row.count(i) == 0:
+                return False
+    return True
+
+
+def solve(board):
+    for pos_y in range(9):
+        for pos_x in range(9):
+            if board[pos_y][pos_x] == 0:
                 for num in range(1, 10):
                     if check_legal(board, pos_x, pos_y, num):
-                        board[pos_x][pos_y] = num
+                        board[pos_y][pos_x] = num
+                        print_board(board)
+                        if is_solved(board):
+                            quit()
+                        solve(board)
+                        board[pos_y][pos_x] = 0
+                return
--- backtracking/test_backtracking.py
+++ backtracking/test_backtracking.py
@@ -16,6 +16,18 @@ class TestBacktracking(unittest.TestCase):
             [5, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
 
+        board_2 = [
+            [1, 0, 4, 0, 2, 0, 0, 0, 0],
+            [0, 3, 0, 0, 0, 9, 0, 7, 1],
+            [0, 9, 6, 0, 0, 0, 4, 0, 5],
+            [5, 0, 1, 0, 6, 0, 0, 0, 0],
+            [7, 0, 8, 9, 1, 2, 3, 0, 4],
+            [3, 0, 9, 4, 0, 5, 6, 1, 8],
+            [0, 1, 0, 0, 0, 0, 7, 0, 0],
+            [0, 0, 3, 7, 0, 4, 0, 0, 0],
+            [0, 0, 0, 2, 3, 0, 0, 4, 6]
+        ]
+
         r = b.check_legal_in_row(board_1, 0, 1)
         self.assertEqual(r, False)
         r = b.check_legal_in_row(board_1, 1, 4)
@@ -24,6 +36,8 @@ class TestBacktracking(unittest.TestCase):
         self.assertEqual(r, True)
         r = b.check_legal_in_row(board_1, 6, 9)
         self.assertEqual(r, True)
+        r = b.check_legal_in_row(board_2, 0, 2)
+        self.assertEqual(r, False)
 
     def test_check_legal_in_col(self):
         board_1 = [
@@ -67,4 +81,57 @@ class TestBacktracking(unittest.TestCase):
         r = b.check_legal_in_box(board_1, 3, 3, 6)
         self.assertEqual(r, False)
         r = b.check_legal_in_box(board_1, 8, 0, 6)
+        self.assertEqual(r, False)
+
+    def test_check_legal(self):
+        board_1 = [
+            [0, 3, 0, 8, 7, 2, 5, 4, 9],
+            [2, 7, 5, 6, 9, 4, 8, 3, 1],
+            [9, 4, 8, 0, 1, 3, 6, 7, 0],
+            [8, 2, 3, 4, 6, 9, 1, 5, 7],
+            [7, 5, 4, 2, 8, 1, 3, 9, 6],
+            [1, 6, 9, 7, 3, 5, 2, 8, 4],
+            [3, 8, 7, 9, 2, 6, 4, 1, 5],
+            [4, 9, 2, 1, 5, 8, 7, 6, 3],
+            [5, 1, 6, 3, 0, 7, 9, 2, 8]
+        ]
+
+        r = b.check_legal(board_1, 3, 2, 5)
+        self.assertEqual(r, True)
+        r = b.check_legal(board_1, 2, 0, 1)
+        self.assertEqual(r, True)
+        r = b.check_legal(board_1, 4, 8, 4)
+        self.assertEqual(r, True)
+        r = b.check_legal(board_1, 0, 0, 1)
+        self.assertEqual(r, False)
+        r = b.check_legal(board_1, 8, 2, 1)
+        self.assertEqual(r, False)
+
+    def test_is_solved(self):
+        board = [
+            [0, 3, 0, 8, 7, 2, 5, 4, 9],
+            [2, 7, 5, 6, 9, 4, 8, 3, 1],
+            [9, 4, 8, 0, 1, 3, 6, 7, 0],
+            [8, 2, 3, 4, 6, 9, 1, 5, 7],
+            [7, 5, 4, 2, 8, 1, 3, 9, 6],
+            [1, 6, 9, 7, 3, 5, 2, 8, 4],
+            [3, 8, 7, 9, 2, 6, 4, 1, 5],
+            [4, 9, 2, 1, 5, 8, 7, 6, 3],
+            [5, 1, 6, 3, 0, 7, 9, 2, 8]
+        ]
+        r = b.is_solved(board)
+        self.assertEqual(r, False)
+
+        board = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [9, 8, 7, 6, 5, 4, 3, 2, 1],
+            [1, 0, 0, 2, 3, 0, 6, 5, 8],
+            [0, 0, 6, 4, 0, 0, 1, 3, 9],
+            [4, 3, 0, 0, 0, 0, 5, 7, 8],
+            [0, 0, 1, 1, 4, 5, 6, 7, 8],
+            [4, 5, 6, 8, 0, 0, 4, 1, 0],
+            [0, 0, 4, 6, 7, 8, 9, 1, 1],
+            [5, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        r = b.is_solved(board)
         self.assertEqual(r, False)
